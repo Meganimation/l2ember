f6487d554a47a8a55251badd6403a315fe2f21f8
Megan Adams <madams23@apple.com>
2022-08-19 14:01:10 -0400

    #09 complete, added form and tracked object

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,11 +1,12 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class CartController extends Controller {
   @service('shoppingCart') cart;
   // subtotal = 0;
   get subtotal() {
-    return this.model.reduce((acc, item) => acc + item.price, 0);
+    return this.cart.itemList.reduce((acc, item) => acc + item.price * item.count, 0);
   }
   // tax = 0;
   get tax() {
@@ -13,5 +14,16 @@ export default class CartController extends Controller {
   }
   get total() {
     return this.subtotal + this.tax;
+  }
+
+  @action
+  updateItemCount(item, event) {
+
+    const count = parseInt(event.target.value);
+
+    if (count <= 0) {
+      item.count = 0;
+    }
+    else item.count = count;
   }
 }
